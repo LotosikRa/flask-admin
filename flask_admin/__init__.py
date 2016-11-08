ca42c85d4831c6af49aa83ee0086e9718f00c183
Illia Ananich <213n.lotos5@gmail.com>
2016-11-08 23:33:51 +0200

    edit `README.rst`, edit author, url, version

--- flask_admin/__init__.py
+++ flask_admin/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.4.2'
-__author__ = 'Flask-Admin team'
-__email__ = 'serge.koval+github@gmail.com'
+__version__ = '1.4.2m1.0.1'
+__author__ = 'Flask-Admin team (with Illia Ananich contributions)'
+__email__ = '213n.lotos5@gmail.com@gmail.com'
 
 
 from .base import expose, expose_plugview, Admin, BaseView, AdminIndexView
